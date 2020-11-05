85df70f6135fb7e4b58460db4457dd14d148753a
lamdiak <lamine.diakit@yahoo.fr>
2020-11-05 23:34:18 +0100

    Initial Commit

---
+++ MessageApp/MessageAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessageAppApp.swift
+//  MessageApp
+//
+//  Created by Lamine DIAKITE on 05/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct MessageAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
